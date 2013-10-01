a35024e8a7d2b5fd3ec15d29b731c2536833da20
Liz Barry <ebarry@gmail.com>
2013-10-01 17:15:29 -0400

    updated README and word changes

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bexperts\b/g, "so-called experts");
+	v = v.replace(/\bIndustry leaders\b/g, "Environmental exploiters");
+	v = v.replace(/\bindustry leaders\b/g, "Environmental exploiters");
 	
 	textNode.nodeValue = v;
 }
