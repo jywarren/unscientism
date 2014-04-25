a239473e0e7f70bc130d83647f94fede4be55997
jywarren <jeff@unterbahn.com>
2014-04-25 15:28:45 -0400

    cleaned up and should be workable now; uploading to Chrome web store as "unscientism"

---
+++ src/content_script.js
@@ -0,0 +1,44 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+    v = v.replace(/\scientist\b/g, '"scientist"');
+	v = v.replace(/\bScientist\b/g, '"Scientist"');
+    v = v.replace(/\expert\b/g, '"expert"');
+	v = v.replace(/\bExpert\b/g, '"Expert"');
+    v = v.replace(/\professional\b/g, '"professional"');
+	v = v.replace(/\bProfessional\b/g, '"Professional"');
+	
+	textNode.nodeValue = v;
+}
+
+
